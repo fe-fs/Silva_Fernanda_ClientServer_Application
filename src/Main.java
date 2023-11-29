c4cf8616237c02c310368df63776617855076560
Fernanda Silva <fe.fs@hotmail.com>
2023-11-28 19:01:22 -0500

    Update Main.java
    fix

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Main {
+/*
+ * Client/Server Application
+ * Name: Fernanda Frederico Ribeiro da Silva
+ * Class: Software Development I CEN-3024C-16046
+ * Professor: Walauskis
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        // Create and show the Server window
+        Server server = new Server();
+        server.start(new Stage());
+
+        // Create and show the Client window
+        Client client = new Client();
+        client.start(new Stage());
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
